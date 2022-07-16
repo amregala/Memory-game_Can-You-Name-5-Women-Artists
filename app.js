391c4bef312a87da9b5bd42a8f50d7bd7e548dae
amregalado <regalado.adriana@gmail.com>
2022-07-16 12:31:03 -0400

    "Flip card effect added"

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("Memory Card Game - Can You Name Five Women Artist?");
+
+const cards = document.querySelectorAll(".memory-card");
+
+function flipCard() {
+  this.classList.toggle("flip");
+}
+
+cards.forEach(card => card.addEventListener("click", flipCard));
