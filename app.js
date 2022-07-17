dbf1fc5cb45505a8a4d89cea7c69ebac3cd63ebe
amregalado <regalado.adriana@gmail.com>
2022-07-17 18:49:17 -0400

    "Adding more images"

--- app.js
+++ app.js
@@ -1,145 +1,154 @@
 console.log("Memory Card Game - Can You Name Five Women Artist?");
 
-
 document.addEventListener("DOMContentLoaded", () => {
+  //grab a couple of things
 
+  const section = document.querySelector("section");
+  const playerLivesCount = document.querySelector("#lives");
+  const scoreCount = document.querySelector("#score");
+  let playerLives = 6;
+  let score = 0;
+  // link text
+  playerLivesCount.textContent = playerLives;
+  scoreCount.textContent = score;
 
-//grab a couple of things
+  // generate the data
+  const getData = [
+    { imgSrc: "imgs/barbara-kruger.jpeg", name: "barbara-kruger" },
+    { imgSrc: "imgs/hayv-kahraman.jpeg", name: "havy-kahraman" },
+    { imgSrc: "imgs/mary-weatherford.WebP", name: "mary-weatherford" },
+    { imgSrc: "imgs/tamara-de-lempicka.webP", name: "tamara-de-lempicka" },
+    { imgSrc: "imgs/zaha-hadid-2.jpeg", name: "zaha-hadid" },
+    { imgSrc: "imgs/alma-thomas.jpeg", name: "alma-thomas" },
+    { imgSrc: "imgs/barbara-kruger.jpeg", name: "barbara-kruger" },
+    { imgSrc: "imgs/hayv-kahraman.jpeg", name: "havy-kahraman" },
+    { imgSrc: "imgs/mary-weatherford.WebP", name: "mary-weatherford" },
+    { imgSrc: "imgs/tamara-de-lempicka.webP", name: "tamara-de-lempicka" },
+    { imgSrc: "imgs/zaha-hadid-2.jpeg", name: "zaha-hadid" },
+    { imgSrc: "imgs/alma-thomas.jpeg", name: "alma-thomas" },
+  ];
+  console.log(getData);
+  console.log(typeof getData);
 
-const section = document.querySelector("section");
-const playerLivesCount = document.querySelector("#lives");
-const scoreCount = document.querySelector("#score");
-let playerLives = 6;
-let score = 0;
-// link text
-playerLivesCount.textContent = playerLives;
-scoreCount.textContent = score;
-// generate the data
-const getData = () => [
-  { imgSrc: "imgs/barbara-kruger.jpeg", name: "barbara-kruger" },
-  { imgSrc: "imgs/hayv-kahraman.jpeg", name: "havy-kahraman" },
-  { imgSrc: "imgs/mary-weatherford.WebP", name: "mary-weatherford" },
-  { imgSrc: "imgs/tamara-de-lempicka.webP", name: "tamara-de-lempicka" },
-  { imgSrc: "imgs/zaha-hadid-2.jpeg", name: "zaha-hadid" },
-  { imgSrc: "imgs/alma-thomas.jpeg", name: "alma-thomas" },
-  { imgSrc: "imgs/barbara-kruger.jpeg", name: "barbara-kruger" },
-  { imgSrc: "imgs/hayv-kahraman.jpeg", name: "havy-kahraman" },
-  { imgSrc: "imgs/mary-weatherford.WebP", name: "mary-weatherford" },
-  { imgSrc: "imgs/tamara-de-lempicka.webP", name: "tamara-de-lempicka" },
-  { imgSrc: "imgs/zaha-hadid-2.jpeg", name: "zaha-hadid" },
-  { imgSrc: "imgs/alma-thomas.jpeg", name: "alma-thomas" },
-];
+  const newImg = [
+    {
+      imgSrc: "imgs/artemisia-gentileschi.webp",
+      name: "artemisia-gentileschi",
+    },
+    { imgSrc: "imgs/yayoi-kusama.webp", name: "yayoi-kusama" },
+  ];
 
-// randomize
-const randomize = () => {
-  const cardData = getData();
-  cardData.sort(() => Math.random() - 0.5);
-  return cardData;
-};
+  getData.push(newImg);
+  console.log(newImg);
+  // randomize
+  const randomize = () => {
+    const cardData = getData;
+    cardData.sort(() => Math.random() - 0.5);
+    return cardData;
+  };
 
-// Card Generator Function
-const cardGenerator = () => {
-  const cardData = randomize();
-  //console.log(cardData);
-  // Generate the HTML
+  // Card Generator Function
+  const cardGenerator = () => {
+    const cardData = randomize();
+    //console.log(cardData);
+    // Generate the HTML
 
-  cardData.forEach(item => {
-    // console.log(item);
-    const card = document.createElement("div");
-    const face = document.createElement("img");
-    const back = document.createElement("div");
-    card.classList = "card";
-    face.classList = "face";
-    back.classList = "back";
-    // Attach the info to the cards
-    face.src = item.imgSrc;
-    card.setAttribute("name", item.name);
+    cardData.forEach(item => {
+      // console.log(item);
+      const card = document.createElement("div");
+      const face = document.createElement("img");
+      const back = document.createElement("div");
+      card.classList = "card";
+      face.classList = "face";
+      back.classList = "back";
+      // Attach the info to the cards
+      face.src = item.imgSrc;
+      card.setAttribute("name", item.name);
 
-    // Attach the cards to the section
-    section.appendChild(card);
-    card.appendChild(face);
-    card.appendChild(back);
+      // Attach the cards to the section
+      section.appendChild(card);
+      card.appendChild(face);
+      card.appendChild(back);
 
-    card.addEventListener("click", e => {
-      card.classList.toggle("toggleCard");
-      checkCards(e);
+      card.addEventListener("click", e => {
+        card.classList.toggle("toggleCard");
+        checkCards(e);
+      });
     });
-  });
-}; // CardGenerator function ends
+  }; // CardGenerator function ends
 
-// Check Cards
-const checkCards = e => {
-  const clickedCard = e.target;
-  clickedCard.classList.add("flipped");
-  const flippedCards = document.querySelectorAll(".flipped");
-  const toggleCard = document.querySelectorAll(".toggleCard");
-  console.log(clickedCard);
+  // Check Cards
+  const checkCards = e => {
+    const clickedCard = e.target;
+    clickedCard.classList.add("flipped");
+    const flippedCards = document.querySelectorAll(".flipped");
+    const toggleCard = document.querySelectorAll(".toggleCard");
+    console.log(clickedCard);
 
-  //Logic
-  if (flippedCards.length === 2) {
-    if (
-      flippedCards[0].getAttribute("name") ===
-      flippedCards[1].getAttribute("name")
-    ) {
-      console.log("match");
-      scoreKeeper();
-      flippedCards.forEach(card => {
-        card.classList.remove("flipped");
-        card.style.pointerEvents = "none";
-      });
-    } else {
-      console.log("wrong");
-      flippedCards.forEach(card => {
-        card.classList.remove("flipped");
-        setTimeout(() => card.classList.remove("toggleCard"), 500);
-      });
-      playerLives--;
-      playerLivesCount.textContent = playerLives;
-      if (playerLives === 0) {
-        restart("🫠...try again");
+    //Logic
+    if (flippedCards.length === 2) {
+      if (
+        flippedCards[0].getAttribute("name") ===
+        flippedCards[1].getAttribute("name")
+      ) {
+        console.log("match");
+        scoreKeeper();
+        flippedCards.forEach(card => {
+          card.classList.remove("flipped");
+          card.style.pointerEvents = "none";
+        });
+      } else {
+        console.log("wrong");
+        flippedCards.forEach(card => {
+          card.classList.remove("flipped");
+          setTimeout(() => card.classList.remove("toggleCard"), 500);
+        });
+        playerLives--;
+        playerLivesCount.textContent = playerLives;
+        if (playerLives === 0) {
+          restart("🫠...try again");
+        }
       }
     }
-  }
 
-  function scoreKeeper() {
-    score += 10;
-    console.log("score function scoreKeeper", score);
-    return (scoreCount.textContent = score);
-    //   return;
-  }
-  // Run a check to see if we won the game
-  if (toggleCard.length === 12) {
-    restart("You're a champ 🏆");
-  }
-};
+    function scoreKeeper() {
+      score += 10;
+      console.log("score function scoreKeeper", score);
+      return (scoreCount.textContent = score);
+      //   return;
+    }
+    // Run a check to see if we won the game
+    if (toggleCard.length === 12) {
+      restart("You're a champ 🏆");
+    }
+  };
 
-// Restart
-const restart = text => {
-  let cardData = randomize();
-  let faces = document.querySelectorAll(".face");
-  let cards = document.querySelectorAll(".card");
-  section.style.pointerEvents = "none";
-  cardData.forEach((item, index) => {
-    cards[index].classList.remove("toggleCard");
-    //Randomize
-    setTimeout(() => {
-      cards[index].style.pointerEvents = "all";
-      faces[index].src = item.imgSrc;
-      cards[index].setAttribute("name", item.name);
-      section.style.pointerEvents = "all";
-    }, 1000);
-  });
+  // Restart
+  const restart = text => {
+    let cardData = randomize();
+    let faces = document.querySelectorAll(".face");
+    let cards = document.querySelectorAll(".card");
+    section.style.pointerEvents = "none";
+    cardData.forEach((item, index) => {
+      cards[index].classList.remove("toggleCard");
+      //Randomize
+      setTimeout(() => {
+        cards[index].style.pointerEvents = "all";
+        faces[index].src = item.imgSrc;
+        cards[index].setAttribute("name", item.name);
+        section.style.pointerEvents = "all";
+      }, 1000);
+    });
 
-  playerLives = 6;
-  playerLivesCount.textContent = playerLives;
-  score = 0;
-  scoreCount.textContent = score;
-  setTimeout(() => window.alert(text), 500);
-};
+    playerLives = 6;
+    playerLivesCount.textContent = playerLives;
+    score = 0;
+    scoreCount.textContent = score;
+    setTimeout(() => window.alert(text), 500);
+  };
 
-cardGenerator();
-
-})
+  cardGenerator();
+});
 // ! ANOTHER VERION THAT WAS BASED OFF OF DIVS IN HTML AND USING FLEX-BOX ORDER TO RANDOMIZE. BUT DIFFICULT TO MOVE THROUGH AND CLUNKY IF I WANTED TO ADD MORE CARDS OR CHANGE OTHER ELEMENTS
 // const allCards = document.querySelectorAll(".memory-card");
 // const card = document.querySelector(".img");
